5abf21399a9f9b926a07fbe3912b6bdf9829c3e7
deivdf <dfdavidfernando@gmail.com>
2025-06-18 18:26:31 -0500

    action

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pollo',
+    version='0.0.1',
+    author='david',
+    author_email='david@example.com',
+    description='A simple web scraping tool',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
